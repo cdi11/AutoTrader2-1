1599a7fb4e95341c15973d4737f175518336763b
Ryan Pliske <ryan.pliske@monsanto.com>
2018-08-01 18:07:33 -0500

    Initial Commit

---
+++ AutoTrader2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoTrader2
+//
+//  Created by PLISKE, RYAN [AG-Contractor/1000] on 8/1/18.
+//  Copyright © 2018 Monsanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
