1eac62b95b27f3e66d9e9e51649ee1ceaac5fb77
Ryan Pliske <ryan.pliske@monsanto.com>
2018-08-29 19:08:41 -0500

    added sort options

--- AutoTrader2/VehicleListViewController.swift
+++ AutoTrader2/VehicleListViewController.swift
@@ -1,49 +1,23 @@
 import UIKit
 
-// Show later
-//let data = try! JSONEncoder().encode(vehicles)
-//let dataAsString = String(data: data, encoding: String.Encoding.utf8)!
-//print(dataAsString)
-protocol SortOption { }
-
-enum PriceSortOption: SortOption {
-    case lowestToHighest
-    case highestToLowest
-}
-enum ModelSortOption: SortOption {
-    case model_aToZ
-    case model_zToA
-}
-enum YearSortOption: SortOption {
-    case newestToOldest
-    case oldestToNewest
-}
-struct YearFilterOption: FilterOption {
-    let minimumYear: Int
-    let maximumYear: Int
-}
-struct PriceFilterOption: FilterOption {
-    let minimumPrice: Double
-    let maximumPrice: Double
-}
-
 class CarsModel {
-    var choseFilterOptions = [FilterOption]() // Initialize to empty array
-    var chosenSortOptions = [SortOption]() // Initialize to empty array
     var vehicles = [Vehicle]()
+    var selections: [Selection] = [
+        Selection(option: .lowestToHighestInPrice, isChecked: false),
+        Selection(option: .aToZForMake, isChecked: false),
+        Selection(option: .aToZForModel, isChecked: false),
+        Selection(option: .oldestToNewest, isChecked: false)
+    ]
     var selectedIndex = 0
     
     init() {
-//        let sortByVehicleOption = VehicleType.fourDoor as! SortOption
-//        let sortByMakeOption = Make.audi as! SortOption
-//        let sortByModelOption = ModelSortOption.model_aToZ
-//        let sortByYearOption = YearSortOption.newestToOldest
-//        self.chosenSortOptions = [sortByVehicleOption, sortByMakeOption, sortByModelOption, sortByYearOption]
-        
-        //        let filterByVehicleOption = VehicleType.fourDoor as! FilterOption
-        
         let generator = VehiclesGenerator()
         self.vehicles = generator.vehicles
+    }
+    
+    func newSelection(at index: Int) {
+        selections[index].isChecked = !selections[index].isChecked
+        // sort the vehicles array!
     }
 }
 
@@ -53,7 +27,7 @@ class CarsCell: UITableViewCell {
     @IBOutlet weak var yearLabel: UILabel!
 }
 
-class ViewController: UIViewController {
+class VehicleListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -66,17 +40,34 @@ class ViewController: UIViewController {
         model = CarsModel()
     }
     
+    deinit {
+        print("vehicle list was deallocated")
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let vehicleViewController = segue.destination as? VehicleDetailViewController else {
-            return
+        if let vehicleViewController = segue.destination as? VehicleDetailViewController  {
+            vehicleViewController.vehicle = model.vehicles[model.selectedIndex]
+        } else if let optionsViewController = segue.destination as? SortOptionsViewController {
+            optionsViewController.delegate = self
         }
-        vehicleViewController.vehicle = model.vehicles[model.selectedIndex]
+        
         super.prepare(for: segue, sender: sender)
     }
     
 }
 
-extension ViewController: UITableViewDelegate {
+extension VehicleListViewController: SortOptionsViewControllerDelegate {
+    var selections: [Selection] {
+        return model.selections
+    }
+    
+    func newSelection(at index: Int) {
+        model.newSelection(at: index)
+    }
+    
+}
+
+extension VehicleListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("🤠 User Selected row at \(indexPath.row)")
         model.selectedIndex = indexPath.row
@@ -84,7 +75,7 @@ extension ViewController: UITableViewDelegate {
     }
 }
 
-extension ViewController: UITableViewDataSource {
+extension VehicleListViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
